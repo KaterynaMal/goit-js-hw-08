b93c0d9cb2c4fe7c092b8c8e0aea73a39f004cdb
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-10-18 19:34:47 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+const key = 'feedback-form-state';
+
+function saveToLocalStorage() {
+  const feedbackData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(key, JSON.stringify(feedbackData));
+}
+
+function loadFromLocalStorage() {
+  const savedData = localStorage.getItem(key);
+  if (savedData) {
+    const feedbackData = JSON.parse(savedData);
+    emailInput.value = feedbackData.email;
+    messageTextarea.value = feedbackData.message;
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const feedbackData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(feedbackData);
+  localStorage.removeItem(key);
+  emailInput.value = '';
+  messageTextarea.value = '';
+}
+
+const updateLocalStorage = throttle(saveToLocalStorage, 500);
+
+form.addEventListener('input', updateLocalStorage);
+form.addEventListener('submit', handleSubmit);
+
+window.addEventListener('load', loadFromLocalStorage);
 

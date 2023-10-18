c86416b1869d7c48cfc9ca8435dd6825b5d06af3
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-10-18 18:50:27 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = new Vimeo('vimeo-player');
+
+function saveCurrentTime(currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+
+function restoreCurrentTime() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    vimeoPlayer.setCurrentTime(currentTime).then(() => {
+      console.log('Video time restored:', currentTime);
+    });
+  }
+}
+
+const updateCurrentTime = throttle((data) => {
+  saveCurrentTime(data.seconds);
+}, 1000);
+
+vimeoPlayer.on('timeupdate', updateCurrentTime);
+vimeoPlayer.ready().then(restoreCurrentTime); 
